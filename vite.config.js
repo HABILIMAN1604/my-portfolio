0d7dd18648dce62ae309be3c7420765ac04d6604
HABILIMAN1604 <152095546+HABILIMAN1604@users.noreply.github.com>
2026-02-02 14:19:46 +0800

    change title and logo title

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/'  // IMPORTANT: GitHub Pages repo name
+  base: '/my-portfolio/'
 })
